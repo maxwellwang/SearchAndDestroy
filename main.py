200c2c1c4f2d417aa824fa98ec8d405f35ea04ab
maxwellwang <maxwellwang01@gmail.com>
2021-03-27 16:56:26 -0400

    implemented maxwell agent, who randomly searches cells until done

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from util import *
 
 map = Map()
 map.print_map()
+result = False
+tries = 0
+while not result:
+    result = map.search((random.randrange(50), random.randrange(50)))
+    tries += 1
+print('Found in ' + str(tries) + ' tries')
--- util.py
+++ util.py
@@ -5,21 +5,11 @@ class Map:
     colors = [u'\u001b[47m', u'\u001b[43m', u'\u001b[42m', u'\u001b[40m']
     reset = u'\u001b[0m'
     red = u'\u001b[31m'
+    probs = [.1, .3, .7, .9]
 
     def __init__(self):
         # randomly generate cell terrain types
-        self.map = [[] for _ in range(50)]
-        for row in self.map:
-            for _ in range(50):
-                rand = random.random()
-                if rand >= .75:
-                    row.append(0)
-                elif rand >= .5:
-                    row.append(1)
-                elif rand >= .25:
-                    row.append(2)
-                else:
-                    row.append(3)
+        self.map = [[random.randrange(4) for _ in range(50)] for _ in range(50)]
         # randomly place target in one of the cells, denoted by adding 4
         self.map[random.randrange(50)][random.randrange(50)] += 4
 
@@ -29,3 +19,15 @@ class Map:
                 # print appropriate terrain color, if target then print red X
                 print(self.colors[cell] + ' ' if cell < 4 else self.colors[cell - 4] + self.red + 'X', end='')
             print(self.reset)
+
+    def search(self, coords):
+        cell = self.map[coords[0]][coords[1]]
+        if cell < 4:
+            return False
+        else:
+            return random.random() < self.probs[cell - 4]
+
+
+class Agent:
+    def __init__(self):
+        print()
