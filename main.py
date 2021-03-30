0a64d9c6a7e90f9faabadf91d49311c31d24560d
maxwellwang <maxwellwang01@gmail.com>
2021-03-30 15:50:25 -0400

    problem 2 done

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from util import *
 
-my_map = Map()
-my_map.print_map()
+a_map = Map()
+a_map.print_map()
+
 found = False
 while not found:
-    found = my_map.search_best_cell()
-    print(my_map.num_searches, my_map.last_searched, my_map.last_result)
-print(my_map.num_searches)
+    found = a_map.search_best_cell(use_found=False)
+    print(a_map.num_searches, a_map.last_searched, a_map.last_result)
+print(a_map.num_searches)
--- util.py
+++ util.py
@@ -9,6 +9,7 @@ class Map:
     red = u'\u001b[31m'
     false_negative_probs = [.1, .3, .7, .9]
     num_searches = 0
+    distance_traveled = 0
     last_searched = (0, 0)
     last_result = False
 
@@ -18,7 +19,13 @@ class Map:
         # randomly place target in one of the cells, denoted by adding 4
         self.map[random.randrange(self.dim)][random.randrange(self.dim)] += 4
         # agent currently believes that each cell has equal chance of having target
-        self.belief = [[(1 / (self.dim * self.dim)) for _ in range(self.dim)] for _ in range(self.dim)]
+        self.belief = [[1 / (self.dim * self.dim) for _ in range(self.dim)] for _ in range(self.dim)]
+        # for each cell, use beliefs and given false negative probs to calculate prob of target being found there
+        self.found_belief = [[self.belief[i][j] * (
+                1 - self.false_negative_probs[self.map[i][j] if self.map[i][j] < 4 else self.map[i][j] - 4]) for j
+                              in range(self.dim)] for i in range(self.dim)]
+        # randomly place agent
+        self.agent = (random.randrange(self.dim), random.randrange(self.dim))
 
     def print_map(self):
         for row in self.map:
@@ -29,6 +36,12 @@ class Map:
 
     def print_belief(self):
         for row in self.belief:
+            for cell in row:
+                print(str(Fraction(cell).limit_denominator()) + ' ', end='')
+            print()
+
+    def print_found_belief(self):
+        for row in self.found_belief:
             for cell in row:
                 print(str(Fraction(cell).limit_denominator()) + ' ', end='')
             print()
@@ -48,37 +61,63 @@ class Map:
                 self.belief[i][j] *= (false_negative_prob if failure_coords == (i, j) else 1) / (
                         (failure_target_belief * false_negative_prob) + (1 - failure_target_belief))
 
+    def update_found_belief(self):
+        for i in range(self.dim):
+            for j in range(self.dim):
+                cell = self.map[i][j]
+                self.found_belief[i][j] = (1 - self.false_negative_probs[cell if cell < 4 else cell - 4]) * \
+                                          self.belief[i][j]
+
     def search(self, coords):
         self.last_searched = coords
         self.num_searches += 1
+        self.distance_traveled += abs(coords[0] - self.agent[0]) + abs(coords[1] - self.agent[1])
+        self.agent = coords
         cell = self.map[coords[0]][coords[1]]
         if cell < 4 or random.random() >= self.false_negative_probs[cell - 4]:
             self.update_belief(coords)
+            self.update_found_belief()
             self.last_result = False
             return False
         else:
             self.last_result = True
             return True
 
-    def best_cell(self):
+    def best_cell(self, use_found):
         max_prob = 0.0
         best_i, best_j = 0, 0
         for i in range(self.dim):
             for j in range(self.dim):
-                if self.belief[i][j] > max_prob:
-                    max_prob = self.belief[i][j]
+                if self.found_belief[i][j] if use_found else self.belief[i][j] > max_prob:
+                    max_prob = self.found_belief[i][j] if use_found else self.belief[i][j]
                     best_i, best_j = i, j
         return best_i, best_j
 
-    def search_best_cell(self):
-        return self.search(self.best_cell())
+    def search_best_cell(self, use_found=False):
+        return self.search(self.best_cell(use_found))
 
 
 class BasicAgent1:
-    def __init__(self):
-        print()
+    def __init__(self, a_map):
+        self.map = a_map
+        self.score = 0
+
+    def run(self):
+        found = False
+        while not found:
+            self.map.search_best_cell(use_found=False)
+        self.score = self.map.distance_traveled + self.map.num_searches
+        print('Score: ' + str(self.score))
 
 
 class BasicAgent2:
-    def __init__(self):
-        print()
+    def __init__(self, a_map):
+        self.map = a_map
+        self.score = 0
+
+    def run(self):
+        found = False
+        while not found:
+            self.map.search_best_cell(use_found=True)
+        self.score = self.map.distance_traveled + self.map.num_searches
+        print('Score: ' + str(self.score))
