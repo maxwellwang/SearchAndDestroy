79f3e1c92f3d13eb90d6f8b3a33202dae54bd589
maxwellwang <maxwellwang01@gmail.com>
2021-03-30 13:16:05 -0400

    problem 1 done

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from util import *
 
 my_map = Map()
 my_map.print_map()
-result = False
-while not result:
-    result = my_map.search_best_cell()
-print('Found in ' + str(my_map.num_searches) + ' searches')
+found = False
+while not found:
+    found = my_map.search_best_cell()
+    print(my_map.num_searches, my_map.last_searched, my_map.last_result)
+print(my_map.num_searches)
--- util.py
+++ util.py
@@ -1,22 +1,24 @@
 import random
+from fractions import Fraction
 
 
 class Map:
+    dim = 50
     colors = [u'\u001b[47m', u'\u001b[43m', u'\u001b[42m', u'\u001b[40m']
     reset = u'\u001b[0m'
     red = u'\u001b[31m'
     false_negative_probs = [.1, .3, .7, .9]
     num_searches = 0
-    observations = []
+    last_searched = (0, 0)
+    last_result = False
 
     def __init__(self):
-        dim = 50
         # randomly generate cell terrain types
-        self.map = [[random.randrange(4) for _ in range(dim)] for _ in range(dim)]
+        self.map = [[random.randrange(4) for _ in range(self.dim)] for _ in range(self.dim)]
         # randomly place target in one of the cells, denoted by adding 4
-        self.map[random.randrange(dim)][random.randrange(dim)] += 4
+        self.map[random.randrange(self.dim)][random.randrange(self.dim)] += 4
         # agent currently believes that each cell has equal chance of having target
-        self.belief = [[1 / (dim * dim) for _ in range(dim)] for _ in range(dim)]
+        self.belief = [[(1 / (self.dim * self.dim)) for _ in range(self.dim)] for _ in range(self.dim)]
 
     def print_map(self):
         for row in self.map:
@@ -28,43 +30,55 @@ class Map:
     def print_belief(self):
         for row in self.belief:
             for cell in row:
-                print(str(round(cell, 2)) + ' ', end='')
+                print(str(Fraction(cell).limit_denominator()) + ' ', end='')
             print()
 
     def update_belief(self, failure_coords):
-        # add new failed search to observations
-        self.observations.append(failure_coords)
+        # we will call the newest failure cell: cell j
+        failure_cell = self.map[failure_coords[0]][failure_coords[1]]
+        # P(Failure in Cell j | Target in Cell j)
+        false_negative_prob = self.false_negative_probs[failure_cell if failure_cell < 4 else failure_cell - 4]
+        # P(Target in Cell j | Observations)
+        failure_target_belief = self.belief[failure_coords[0]][failure_coords[1]]
         # for each cell i, update our belief P(Target in Cell i | Observations and Failure in Cell j)
-        dim = len(self.belief)
-        for i in range(dim):
-            for j in range(dim):
-                print(i, j)
+        # = P(Failure in Cell j | Target in Cell i) * P(Target in Cell i | Observations) / P(Failure in Cell j)
+        # ^ see writeup for how we arrived at this
+        for i in range(self.dim):
+            for j in range(self.dim):
+                self.belief[i][j] *= (false_negative_prob if failure_coords == (i, j) else 1) / (
+                        (failure_target_belief * false_negative_prob) + (1 - failure_target_belief))
 
     def search(self, coords):
+        self.last_searched = coords
         self.num_searches += 1
         cell = self.map[coords[0]][coords[1]]
-        if cell < 4 or not random.random() < self.false_negative_probs[cell - 4]:
+        if cell < 4 or random.random() >= self.false_negative_probs[cell - 4]:
             self.update_belief(coords)
+            self.last_result = False
             return False
         else:
+            self.last_result = True
             return True
 
     def best_cell(self):
         max_prob = 0.0
         best_i, best_j = 0, 0
-        dim = len(self.belief)
-        for i in range(dim):
-            for j in range(dim):
-                print(i, j, self.belief[i][j])
+        for i in range(self.dim):
+            for j in range(self.dim):
                 if self.belief[i][j] > max_prob:
                     max_prob = self.belief[i][j]
                     best_i, best_j = i, j
         return best_i, best_j
 
     def search_best_cell(self):
-        self.search(self.best_cell())
+        return self.search(self.best_cell())
 
 
-class Agent:
+class BasicAgent1:
+    def __init__(self):
+        print()
+
+
+class BasicAgent2:
     def __init__(self):
         print()
